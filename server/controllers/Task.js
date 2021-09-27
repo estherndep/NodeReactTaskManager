1026951d243996609286bb52535d32032ea402b4
esther ndep <esther.ndep-cic-uk@ibm.com>
2021-09-27 09:35:16 +0100

    create POST /tasks/create endpoint #2

--- server/controllers/Task.js
+++ server/controllers/Task.js
@@ -13,6 +13,21 @@ const TaskController = {
                 message: 'somethinhg went wrong'
             })
         }
+    },
+
+    async createTask (req,res,next) {
+        try {
+            const newTask = await TaskModel.createTask(req.body.description)
+            
+            return res.status(201).json({
+                message: 'created successfully',
+                data: newTask
+            })
+        } catch (error) {
+            return res.status(500).json({
+                message: 'somethinhg went wrong'
+            })
+        }   
     }
 }
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,4 +5,6 @@ const TaskController = require('../controllers/Task')
 
 router.get('/tasks', TaskController.getTaskList)
 
+router.post('/tasks/create', TaskController.createTask)
+
 module.exports = router
