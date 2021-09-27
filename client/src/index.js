5d09b5f46b5ddf68f0b906c4732026d186acc5ac
esther ndep <esther.ndep-cic-uk@ibm.com>
2021-09-27 08:20:43 +0100

    Create client base

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/style.scss';
+import App from './containers/App'
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector(
+      '#App'
+    )
+);
---
+++ client/webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env'],
+                        plugins: ['@babel/plugin-transform-runtime']
+                    },
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:'./public/index.html'
+        }
+    )]
+};
