799c6e110a33cf1c297fa3e5b3264aa110e38b0d
esther ndep <esther.ndep-cic-uk@ibm.com>
2021-09-27 09:27:57 +0100

    write integration test #2

--- server/tests/integration/taskApiRoutes.test.js
+++ server/tests/integration/taskApiRoutes.test.js
@@ -18,3 +18,37 @@ describe(`GET ${baseURL}`, () => {
         expect(response.statusCode).toBe(200)
     })
 })
+
+describe(`POST ${baseURL}/create`, () => {
+    describe('given a description', () => {
+        it('should return 201 status code',async () => {
+            const response = await request(app)
+                .post(`${baseURL}/create`)
+                .send(JSON.parse(validTaskEntry))
+
+            expect(response.statusCode).toBe(201)
+        })
+        
+        it('should respond with a JSON object of new uncompleted task', async () => {
+            const response = await request(app)
+                .post(`${baseURL}/create`)
+                .send(JSON.parse(validTaskEntry))
+
+
+            taskFields.forEach(item => {
+                expect(response.body.data).toHaveProperty(item)
+            })
+            expect(response.body.data.completed).toBe(false)
+        })
+    })
+
+    describe('given an invalid request body', () => {
+        it('should return 400 status code',async () => {
+            const response = await request(app)
+                .post(`${baseURL}/create`)
+                .send({})
+
+            expect(response.statusCode).toBe(400)
+        })
+    })
+})
