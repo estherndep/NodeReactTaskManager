028fcd01c2fe7b29ad2b8eceeac6043eeafacfb8
esther ndep <esther.ndep-cic-uk@ibm.com>
2021-09-27 08:46:58 +0100

    write integration test for api #1

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,8 @@
 const express =  require('express')
 const router = express.Router()
 
+const TaskController = require('../controllers/Task')
+
+router.get('/tasks', TaskController.getTaskList)
 
 module.exports = router
---
+++ server/tests/data.js
@@ -0,0 +1,15 @@
+exports.validTaskEntry = JSON.stringify({
+    description:"Test task description"
+})
+
+exports.createdTask = {
+    id: 12345,
+    description: "Test task description",
+    completed: false
+}
+
+exports.completedTask = {
+    id: 123456,
+    description: "Test task description",
+    completed: true
+}
